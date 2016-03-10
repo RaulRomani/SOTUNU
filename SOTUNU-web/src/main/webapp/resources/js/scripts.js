1ffba135e11000073a58f47903d247537c13cff7
Raúl <romanidev@gmail.com>
2016-03-10 06:09:01 -0500

    Change locale calendar primefaces
    Add locale in a script, load in .xhtml and set locale=es property of p:calendar

--- SOTUNU-web/src/main/webapp/resources/js/scripts.js
+++ SOTUNU-web/src/main/webapp/resources/js/scripts.js
@@ -1,4 +1,33 @@
+
+
 $(document).ready(function () {
+  
+  PrimeFaces.locales['es'] = {
+    closeText: 'Cerrar',
+    prevText: 'Anterior',
+    nextText: 'Siguiente',
+    monthNames: ['Enero','Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
+    dayNames: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
+    dayNamesShort: ['Dom','Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+    dayNamesMin: ['D','L','M','X','J','V','S'],
+    weekHeader: 'Semana',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: '',
+    timeOnlyTitle: 'Sólo hora',
+    timeText: 'Tiempo',
+    hourText: 'Hora',
+    minuteText: 'Minuto',
+    secondText: 'Segundo',
+    currentText: 'Fecha actual',
+    ampm: false,
+    month: 'Mes',
+    week: 'Semana',
+    day: 'Día',
+    allDayText : 'Todo el día'
+};
 
   function readURL(input) {
     if (input.files && input.files[0]) {
@@ -37,4 +66,7 @@ $(document).ready(function () {
 });
 
 
-  
+
+
+
+
